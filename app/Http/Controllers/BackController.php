076dd4089f268beafcfed76ee6c4f04e4399bf43
Milos Tosic <milostosic1125@outlook.com>
2019-01-08 00:19:56 +0100

    add update function in BackController

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -17,17 +17,17 @@ class BackController extends Controller
     {
         $videos = Video::all();
         list($timestamp, $signature) = $this->genCloudinaryAuthSignature();
-        return view('videos', compact('videos', 'timestamp','signature'));
+        return view('videos', compact('videos', 'timestamp', 'signature'));
     }
 
     public function create(Request $request)
     {
         $this->validate($request, [
-         'title' => 'required|max:255',
-         'description' => 'required',
-         'date' => 'required|date',
-         'videoId' => 'required',
-         'coverId' => 'required'
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'date' => 'required|date',
+            'videoId' => 'required',
+            'coverId' => 'required'
         ]);
 
         $this->saveVideo($request);
@@ -36,13 +36,37 @@ class BackController extends Controller
 
     }
 
+
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'date' => 'required|date',
+            'videoId' => 'required',
+            'coverId' => 'required'
+        ]);
+
+        $video = Video::where('id', $request->input("id"))->firstOrFail();
+        $video->update([
+            "title" => $request->input("title"),
+            "description" => $request->input("description"),
+            "videoId" => $request->input("videoId"),
+            "videoUrl" => $request->input("videoUrl"),
+            "coverId" => $request->input("coverId"),
+            "coverUrl" => $request->input("coverUrl"),
+            "date" => new \DateTime($request->input("date")),
+            "slug" => str_slug($video->title, "-")
+        ]);
+    }
+
     public function saveVideo(Request $request)
     {
         $video = new Video();
         $video->title = $request->input("title");
         $video->description = $request->input("description");
-        $video->videoId =  $request->input("videoId");
-        $video->videoUrl =  $request->input("videoUrl");
+        $video->videoId = $request->input("videoId");
+        $video->videoUrl = $request->input("videoUrl");
         $video->coverId = $request->input("coverId");
         $video->coverUrl = $request->input("coverUrl");
         $video->date = new \DateTime($request->input("date"));
@@ -56,9 +80,10 @@ class BackController extends Controller
         return redirect('/backoffice')->with('status', 'Video Deleted Successfully');
     }
 
-    public function genCloudinaryAuthSignature() {
-      $timestamp = time();
-      $string = "cloud_name=".env('CLOUDINARY_CLOUD_NAME')."&timestamp=".time()."&username=".env('CLOUDINARY_USER_NAME').env('CLOUDINARY_API_SECRET');
-      return [$timestamp, hash('sha256', $string)];
+    public function genCloudinaryAuthSignature()
+    {
+        $timestamp = time();
+        $string = "cloud_name=" . env('CLOUDINARY_CLOUD_NAME') . "&timestamp=" . time() . "&username=" . env('CLOUDINARY_USER_NAME') . env('CLOUDINARY_API_SECRET');
+        return [$timestamp, hash('sha256', $string)];
     }
 }
