701ea6c4309a8e35d83f09c20c7bbf7d050df65a
xinhonglee <xinhong1010@outlook.com>
2019-02-01 14:26:12 +0100

    refonte page corpo

--- resources/views/corporate.blade.php
+++ resources/views/corporate.blade.php
@@ -4,68 +4,212 @@
   <header class="banner">
     <amp-img class="cover" layout="fill" src="/assets/banner1.jpg"></amp-img>
     <div class="title">
-      <h1><span>Regarder.</span>
-      <br />
-      <span>Comprendre.<span>
-      <br />
-      <span>Partager.<span></h1>
-    </div>
+      <h1>Regarder. Comprendre. Partager.</h1>
+	  <ul class="social">
+        <li>
+			<a href="#">
+				<amp-img src="/assets/facebook_green.svg" media="(max-width: 52rem)" width="13" height="25" layout="fixed"></amp-img>
+				<amp-img src="/assets/facebook_green.svg" media="(min-width: 52.06rem)" width="34" height="66" layout="fixed"></amp-img>				
+			</a>
+		</li>
+        <li>
+			<a href="#">
+				<amp-img src="/assets/twitter_green.svg"  media="(max-width: 52rem)" width="27" height="23" layout="fixed"></amp-img>
+				<amp-img src="/assets/twitter_green.svg" media="(min-width: 52.06rem)" width="75" height="64" layout="fixed"></amp-img>
+			</a>
+		</li>
+        <li>
+			<a href="#">
+				<amp-img src="/assets/youtube_green.svg"  media="(max-width: 52rem)" width="34" height="25" layout="fixed"></amp-img>
+				<amp-img src="/assets/youtube_green.svg" media="(min-width: 52.06rem)" width="92" height="66" layout="fixed"></amp-img>
+			</a>
+		</li>
+        <li>
+			<a href="#">
+				<amp-img src="/assets/instagram_green.svg"  media="(max-width: 52rem)" width="24" height="24" layout="fixed"></amp-img>
+				<amp-img src="/assets/instagram_green.svg" media="(min-width: 52.06rem)" width="65" height="65" layout="fixed"></amp-img>
+			</a>
+		</li>
+        <li>
+			<a href="#">
+				<amp-img src="/assets/snapchat_green.svg"  media="(max-width: 52rem)" width="27" height="26" layout="fixed"></amp-img>
+				<amp-img src="/assets/snapchat_green.svg" media="(min-width: 52.06rem)" width="71" height="67" layout="fixed"></amp-img>
+			</a>
+		</li>
+      </ul>
+	</div>
   </header>
 
   <section class="intro">
     <div class="clearfix mxn2 ">
-      <div class="sm-col sm-col-4 px2 center">
-        <amp-img src="/assets/phone.png" width="263" height="714" layout="fixed"></amp-video>
-      </div>
+      
       <div class="sm-col sm-col-8 px2 mt4">
         <h3>Des images pour écrire un monde en mouvement.</h3>
         <p>Conçu pour les écrans et les réseaux sociaux, Loopsider raconte et décrypte en images les histoires qui font l’actualité. Celles qui vous touchent et qui vous inspirent, celles qui vous rendent curieux et qui vous connectent aux autres. Ici. Maintenant.</p>
-        <ul class="social">
-          <a href="#"><li><amp-img src="/assets/facebook_green.png" width="13" height="24" layout="fixed"></amp-img></li></a>
-          <a href="#"><li><amp-img src="/assets/twitter_green.png" width="27" height="24" layout="fixed"></amp-img></li></a>
-          <a href="#"><li><amp-img src="/assets/youtube_green.png" width="34" height="24" layout="fixed"></amp-img></li></a>
-          <a href="#"><li><amp-img src="/assets/insta_green.png" width="25" height="24" layout="fixed"></amp-img></li></a>
-          <a href="#"><li><amp-img src="/assets/snap_green.png" width="27" height="24" layout="fixed"></amp-img></li></a>
-        </ul>
+
+      </div>
+	  <div class="sm-col sm-col-4 px2 center mt2">
+        <amp-img src="/assets/phone.png" width="263" height="714" layout="fixed"></amp-video>
       </div>
   </section>
 
   <section class="chiffres">
-    <h2>Chiffres</h2>
-    <ul>
-      <li><span class="number">50</span> <span class="unit">millions</span> vidéos vues / mois</li>
-      <li><span class="number">500 000</span> followers sur les réseaux</li>
-      <li><span class="number">2</span> <span class="unit">millions</span> de reach hebdomadaire</li>
-      <li><span class="number">50</span><span class="unit">%</span> de notre audience a moins de 25 ans</li>
-    </ul>
+	<div class="content">
+		<h2>En chiffres</h2>
+		<p class="subtitle">La plus forte croissance d'un média sur facebook en 2018.</p>
+		<div class="clearfix border">
+			<div class="sm-col sm-col-3 border">
+				<div class="number">
+					30 
+				</div>
+				<div class="unit">
+					millions
+				</div>
+				<div class="comment">
+					de personnes touchées<br />chaque mois
+				</div>
+			</div>
+			<div class="sm-col sm-col-3 border">
+				<div class="number">
+					3
+				</div>
+				<div class="unit">
+					millions
+				</div>
+				<div class="comment">
+					de personnes touchées<br />chaque jour
+				</div>
+			</div>
+			<div class="sm-col sm-col-3 border">
+				<div class="number">
+					50
+				</div>
+				<div class="unit">
+					millions
+				</div>
+				<div class="comment">
+					de vidéos vues<br />chaque mois
+				</div>
+			</div>
+			<div class="sm-col sm-col-3 border">
+				<div class="number">
+					1
+				</div>
+				<div class="unit">
+					million
+				</div>
+				<div class="comment">
+					d'interactions uniques<br />chaque mois
+				</div>
+			</div>
+		</div>
+		<p class="center tagline-green">
+			Loopsider
+		</p>
+		<p class="center tagline">
+			un enjeu
+		</p>
+		<p class="subtitle">
+			De l’attention à l’engagement,
+			<br />
+			de l’engagement au partage
+		</p>
+	</div>
+	<div class="footer-img">
+		<div class="gradient"></div>
+		<amp-img layout="responsive" width="1680" height="994" src="/assets/background_footer.jpg"></amp-img>
+	</div>
   </section>
+  
+  <section class="contents">
+	<h3 class="center">Nous produisons des contenus qui créent la conversation chez les Millenials</h3>
+	<div class="clearfix mxn2">
+		<div class="sm-col sm-col-4 px2">	
+			<div class="cell">
+				<amp-img layout="fixed" width="56" height="56" src="/assets/facebook_black.svg"></amp-img>
+				<p>Notre équipe crée et produit des contenus pour les marques.</p>
+			</div>
+		</div>
+		<div class="sm-col sm-col-4 px2">	
+			<div class="cell">
+				<amp-img layout="fixed" width="57" height="56" src="/assets/facebook_black.svg"></amp-img>
+				<p>Via ces contenus, nous permettons aux marques d’avoir une voix dans cette conversation.</p>
+			</div>
+		</div>
+		<div class="sm-col sm-col-4 px2">	
+			<div class="cell">
+				<amp-img layout="fixed" width="56" height="56" src="/assets/facebook_black.svg"></amp-img>
+				<p>Nous mettons notre expertise du storytelling visuel au service de nos partenaires pour générer de l’engagement au meilleur coût.</p>
+			</div>
+		</div>
+	</div>
+  </section>
+  
+  <section class="work">
+	<h2>Comment travailler ensemble ?</h2>
+	<div class="clearfix table">
+		<div class="sm-col sm-col-6">	
+			<div class="cell-header">
+				<p class="center">Média</p>
+			</div>
+			<div class="cell">
+				<ul>
+					<li>Nous créons et produisons des contenus originaux que nous distribuons à nos communautés.</li>
+					<li>Nous déclinons ces contenus pour les adapter aux formats et aux codes des différents réseaux sociaux.</li>
+					<li>Et nous optimisons la distribution de ces contenus.</li>
+				</ul>
+			</div>
+		</div>
+		<div class="sm-col sm-col-6">	
+			<div class="cell-header">
+				<p class="center">Marque Blanche</p>
+			</div>
+			<div class="cell">
+				<ul>
+					<li>Bénéficiez de notre expertise des médias sociaux pour servir vos objectifs de communication interne ou externe.</li>
+					<li>Le savoir-faire d’un média, la flexibilité d’une agence.</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<h4 class="center">Nos clients</h4>
+	<ul class="customers">
+		<li><amp-img layout="fixed" width="56" height="56" src="/assets/facebook_green.svg"></amp-img></li>
+		<li><amp-img layout="fixed" width="56" height="56" src="/assets/facebook_green.svg"></amp-img></li>
+		<li><amp-img layout="fixed" width="56" height="56" src="/assets/facebook_green.svg"></amp-img></li>
+	</ul>
+	<div class="center">
+		<a href="#" class="btn">Nous contacter</a>
+	</div>
+  </section>  
 
   <section class="team">
-    <h2>Team</h2>
-    <div class="clearfix mxn2 ">
-      <div class="sm-col sm-col-6 px2 photo">
-        <amp-img src="/assets/johan.png" width="540" height="670" layout="responsive"></amp-img>
-        <h4>Johan Hufnagel</h4>
-        <p>Directeur de la rédaction</p>
-      </div>
-      <div class="sm-col sm-col-6 px2 photo">
-        <amp-img src="/assets/daphnee.png" width="540" height="670" layout="responsive"></amp-img>
-        <h4>Daphnée Denis</h4>
-        <p>Rédactrice en chef</p>
-      </div>
-    </div>
-    <div class="clearfix mxn2 ">
-      <div class="sm-col sm-col-6 px2 photo">
-        <amp-img src="/assets/giuseppe.png" width="540" height="670" layout="responsive"></amp-img>
-        <h4>Giuseppe de Martino</h4>
-        <p>CEO</p>
-      </div>
-      <div class="sm-col sm-col-6 px2 photo">
-        <amp-img src="/assets/arnaud.png" width="540" height="670" layout="responsive"></amp-img>
-        <h4>Arnaud Maillard</h4>
-        <p>COO</p>
-      </div>
-    </div>
+	<amp-img class="cover xs-hide sm-hide" layout="fill" src="/assets/banner1.jpg"></amp-img>
+	<div class="white">
+		<h2>Team</h2>
+		<div class="clearfix mxn2 ">
+		  <div class="col col-6 sm-col sm-col-3 px2 photo">
+			<amp-img src="/assets/johan.png" width="540" height="670" layout="responsive"></amp-img>
+			<h4>Johan Hufnagel</h4>
+			<p>Directeur de la rédaction</p>
+		  </div>
+		  <div class="col col-6 sm-col sm-col-3 px2 photo">
+			<amp-img src="/assets/daphnee.png" width="540" height="670" layout="responsive"></amp-img>
+			<h4>Daphnée Denis</h4>
+			<p>Rédactrice en chef</p>
+		  </div>
+		  <div class="col col-6 sm-col sm-col-3 px2 photo">
+			<amp-img src="/assets/giuseppe.png" width="540" height="670" layout="responsive"></amp-img>
+			<h4>Giuseppe de Martino</h4>
+			<p>CEO</p>
+		  </div>
+		  <div class="col col-6 sm-col sm-col-3 px2 photo">
+			<amp-img src="/assets/arnaud.png" width="540" height="670" layout="responsive"></amp-img>
+			<h4>Arnaud Maillard</h4>
+			<p>COO</p>
+		  </div>
+		</div>
+	</div>
   </section>
 
   <section class="newsletter">
--- resources/views/css/corporate.blade.php
+++ resources/views/css/corporate.blade.php
@@ -20,16 +20,25 @@
      url('/assets/rational-bookbold-webfont.woff') format('woff');
   font-weight: normal;
   font-style: normal;
+}
 
+@font-face {
+  font-family: 'rationalmedium';
+  src: url('/assets/rational-medium.woff2') format('woff2'),
+     url('/assets/rational-medium.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
 }
 
 body {
   background-color: #5feb98;
   color: #262626;
 }
+
 h1, h2, h3, h4, h5, h6 {
 	letter-spacing: normal;
 }
+
 a, a:visited, a:hover {
   text-decoration: none;
 }
@@ -54,7 +63,7 @@ h2.section {
 .ampstart-headerbar {
   z-index: 200;
   background: linear-gradient(to bottom, rgba(0,0,0,0.5) 0%,rgba(0,0,0,0) 100%); /* W3C */
-  height: 250px;
+  height: 175px;
 }
 
 .ampstart-headerbar .ampstart-navbar-trigger {
@@ -103,51 +112,16 @@ amp-img.cover img {
   width: 100%;
   background-color: #5feb98;
   color: #262626;
+  padding: 0;
+  margin: 0;
 }
 #menu .close {
   cursor:pointer;
-}
-@media (max-width:60rem) {
-  #menu .close {
     font-family: 'rationalbook_bold';
     font-size: 18px;
     position: absolute;
     top: 30px;
     right: 30px;
-  }
-}
-@media (min-width:60.06rem) {
-  #menu .close {
-    background-color: #262626;
-    width: 100px;
-    height: 100px;
-    position: absolute;
-    top: 55px;
-    right: 55px;
-  }
-  #menu .close:hover {
-    opacity: 1;
-  }
-  #menu .close:before, .close:after {
-    position: absolute;
-    left: 50px;
-    top: 40px;
-    content: ' ';
-    height: 20px;
-    width: 2px;
-    background-color: #5feb98;
-  }
-  #menu .close:before {
-    transform: rotate(45deg);
-  }
-  #menu .close:after {
-    transform: rotate(-45deg);
-  }
-}
-@media (min-width:60.06rem) {
-  #menu {
-    height: 220px;
-  }
 }
 #menu nav ul {
   list-style-type: none;
@@ -156,41 +130,7 @@ amp-img.cover img {
 #menu nav ul li a, #menu nav ul li a:visited, #menu nav ul li a:hover {
   text-decoration: none;
 }
-@media (max-width:60rem) {
-  #menu nav ul {
-    margin-top: 135px;
-    padding-left: 30px;
-    padding-right: 30px;
-  }
-  #menu nav ul li {
-    width: 100%;
-    border-bottom: solid 10px #262626;
-    font-size: 44px;
-    line-height: 50px;
-    margin-bottom: 30px;
-  }
-}
-@media (min-width:60.06rem) {
-  #menu nav ul {
-    margin-top: 70px;
-    padding-left: 70px;
-    padding-right: 70px;
-  }
-  #menu nav ul li {
-    display:inline;
-    border-bottom: solid 10px #262626;
-    font-size: 52px;
-    line-height: 48px;
-    margin-right: 80px;
-  }
-}
 
-#menu .menu-social {
-  position: absolute;
-  bottom: 40px;
-  width: 100%;
-  text-align: center;
-}
 #menu .menu-social ul {
   list-style-type: none;
   margin: 0;
@@ -198,9 +138,111 @@ amp-img.cover img {
 }
 #menu .menu-social ul li {
   display: inline-block;
-  width: 15%;
   margin: 0;
   padding: 0;
+}
+
+@media (max-width:52.02rem) {
+  #menu .close {
+	  color: #fff;
+  }
+  #menu nav ul {
+	   padding-left: 0;
+	   margin: 0;
+  }
+  #menu nav.dark ul {
+	  padding-top: 45px;	 
+	  background-color: #262626;
+	  color: #fff;
+	  font-family: 'rationalmedium';	  
+  }
+  #menu nav ul li {
+    width: 100%;
+    border-bottom: solid 1px #aaa;
+    font-size: 25px;
+    line-height: 72px;
+	padding-left: 30px;
+  }
+  #menu nav ul.dark li {
+	font-size: 21px;
+	border-bottom: solid 1px #ccc;
+  }
+  #menu h4 {
+	  font-family: 'rationalsemi_bold';
+	  font-weight: 15px;
+	  padding-left: 30px;
+	  text-transform: uppercase;
+	  padding-top: 40px;
+	  padding-bottom: 10px;
+  }
+  #menu .menu-social {
+	  margin-bottom: 30px;
+	  width: 100%;
+	  text-align: center;
+	  margin-top: 50px;
+	}
+  #menu .menu-social ul li {
+	  width: 15%;
+  }
+}
+@media (min-width:52.02rem) {
+  #menu amp-img.cover img {
+	object-fit: cover;
+	opacity: .35;
+  }
+  #menu .logo {
+    top: 55px;
+    left: 55px;
+  }
+  #menu .close {
+	top: 45px;
+	right: 55px;
+	font-size: 22px;
+	margin-top: 10px;
+	color: #e4e5e6;
+	line-height: 1;
+  }
+  #menu {
+	background-color: #262626;
+	color: #fff;
+  }
+  #menu nav.dark ul {
+	position: absolute;
+    bottom: 45px;
+	left: 55px;
+	padding: 0;
+	margin: 0;
+  }
+  #menu nav.dark ul li {
+    display:inline;
+	font-family: 'rationalsemi_bold';
+    border-bottom: solid 2px #5feb98;
+    font-size: 1.19vw; /* 20px */
+	line-height: 42px;
+    margin-right: 20px;
+	color: #e4e5e6;
+  }
+  #menu nav.category ul {
+	position: absolute;
+    top: 35px;
+	left: 40vw; 
+  }
+  #menu nav.category ul li {
+	font-family: 'rationalextra_bold';
+    font-size: 3vw;    /* 72px */
+    line-height: calc((100vh - 70px) / 9);
+  }
+  #menu nav.category ul li a:hover{
+	border-bottom: solid 10px #5feb98; 
+  }
+  #menu .menu-social {
+	position: absolute;
+    bottom: 52px;
+	right: 55px;
+  }
+  #menu .menu-social ul li {
+	margin-left: 50px;
+  }
 }
 
 footer {
@@ -212,72 +254,119 @@ footer {
 }
 @media (max-width:40rem) {
   footer {
-    padding: 30px;
+    padding: 0 30px 30px 30px;
     font-size: 14px;
   }
+}
+footer nav {
+  border-top: solid 10px #5feb98;
+  color: #5feb98;
+  font-family: 'rationalsemi_bold';
+  font-size: 18px;
+  line-height: 36px;
+}
+footer nav ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 20px 0 20px 0;
 }
 
 .banner {
   position: relative;
   width: 100%;
   height: 1024px;
+  background-color: #262626;
+  text-align: center;
+}
+
+.banner .cover {
+  opacity: 0.5;
 }
 
 .banner .title {
   position: absolute;
-  left: 290px;
-  right: 290px;
-  top: 388px;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 0; 
+  right: 0; 
+  margin-left: auto; 
+  margin-right: auto; 
 }
 
-.banner .title h1 span {
+.banner .title h1 {
   font-family: 'rationalextra_bold';
-  font-size: 110px;
-  line-height: 160px;
-  color: #ffffff;
-  border-bottom: solid 20px #5feb98;
+  font-size: 60px;
+  line-height: 1.13;
+  color: #fff;
 }
 
-@media (max-width:60rem) {
+@media (max-width:40rem) {
   .banner {
-    height: 360px;
+    height: 600px;
   }
 
-  .banner .title {
-    left: 30px;
-    right: 30px;
-    top: 135px;
+  .banner .title h1 {
+	font-size: 30px;
+	margin-left: 20px;
+	margin-right: 20px;
   }
+}
 
-  .banner .title h1 span {
-    font-size: 40px;
-    line-height: 50px;
-    border-bottom: solid 10px #5feb98;
-  }
-
+.banner .title .social {
+  list-style-type: none;
+  margin-top: 40px;
+  padding: 0;
+}
+.banner .title .social li {
+  display: inline-block;
+  text-align: center;
+  padding: 0 6vw 0 6vw;
+}
+@media (max-width:52rem) {
+	.banner .title .social li {
+	  padding: 0 5vw 0 5vw;
+	}
 }
 
 section {
   padding: 85px 290px 85px 290px;
 }
 
-@media (max-width:60rem) {
+@media (max-width:52rem) {
   section {
     padding: 35px 30px 35px 30px;
   }
 }
 
+section.chiffres {
+  padding: 0;
+}
+
+section.chiffres .content {
+  padding: 85px 290px 260px 290px;
+}
+
+@media (max-width:52rem) {
+  section.chiffres .content {
+    padding: 35px 30px 130px 30px;
+  }
+}
+
 section h2 {
   font-family: 'rationalextra_bold';
-  font-size: 110px;
-  line-height: 110px;
+  font-size: 80px;
+  line-height: 90px;
   border-bottom: solid 20px #5feb98;
+  margin-top: 100px;
+  margin-bottom: 100px;
 }
 
 section h3 {
   font-family: 'rationalextra_bold';
-  font-size: 76px;
-  line-height: 80px;
+  font-size: 60px;
+  line-height: 64px;
+  margin-top: 135px;
+  margin-bottom: 20px;
 }
 
 section h4 {
@@ -289,21 +378,32 @@ section h4 {
 }
 
 section p {
-  font-family: 'rationalbook_bold';
+  font-family: 'rationalsemi_bold';
   font-size: 30px;
   line-height: 42px;
 }
 
-@media (max-width:60rem) {
+section p.subtitle {
+  font-family: 'rationalmedium';
+  font-size: 42px;
+  line-height: 56px;
+  text-align: center;
+  margin-bottom: 80px;
+}
+
+@media (max-width:52rem) {
   section h2 {
-    font-size: 44px;
-    line-height: 50px;
+    font-size: 36px;
+    line-height: 40px;
     border-bottom: solid 10px #5feb98;
+	margin-top: 40px;
+	margin-bottom: 30px;
   }
 
   section h3 {
-    font-size: 38px;
-    line-height: 38px;
+    font-size: 30px;
+    line-height: 34px;
+	margin-top: 10px;
     margin-bottom: 20px;
   }
 
@@ -315,33 +415,16 @@ section p {
   }
 
   section p {
-    font-size: 28px;
-    line-height: 30px;
+    font-size: 20px;
+    line-height: 26px;
   }
-}
-
-.social {
-  list-style-type: none;
-  margin-top: 20px;
-  padding: 0;
-}
-.social li {
-  display: inline-block;
-  background-color: #262626;
-  width: 60px;
-  height: 60px;
-  padding: 18px 0 0 0;
-  text-align: center;
-}
-@media (max-width:60rem) {
-  .social {
-    text-align: center;
-  }
-  .social li {
-    width: 48px;
-    height: 48px;
-    padding: 12px 0 0 0;
-  }
+  
+  section p.subtitle {
+	  font-size: 22px;
+	  line-height: 32px;
+	  text-align: left;
+	  margin-bottom: 20px;
+	}
 }
 
 .contact {
@@ -349,80 +432,304 @@ section p {
 }
 
 .chiffres {
-  background: #262626 url('/assets/banner2.jpg') no-repeat center center;
-  background-size: cover;
-  padding-bottom: 700px;
+  background: #262626;
+  color: #fff;
+  position: relative;
 }
 
-.chiffres ul {
-  padding: 0;
-  margin-top: 50px;
+.chiffres .border {
+	border-color: #4c4c4c;
 }
 
-.chiffres ul li {
-  list-style-type: none;
+.chiffres .number {
+  font-size: 130px;
+  line-height: 1;
+  color: #5feb98;
+  font-family: 'rationalextra_bold';
+  text-align: center;
+  padding-top: 100px;
+}
+
+.chiffres .unit {
+  color: #5feb98;
+  font-size: 30px;
+  line-height: 0.4;
+  font-family: 'rationalsemi_bold';
+  text-align: center;
+  padding-bottom: 100px;
+  border-bottom: solid 2px #4c4c4c;
+}
+
+.chiffres .comment {
   color: #e4e5e6;
-  font-size: 42px;
-  font-family: 'rationalbook_bold';
+  font-size: 19px;
+  font-family: 'rationalsemi_bold';
+  text-align: center;
+  padding-top: 50px;
+  padding-bottom: 50px;
 }
 
-.chiffres ul li .number {
-  font-size: 110px;
+.chiffres .footer-img {
+	width: 100%;
+	position: absolute;
+	bottom: 0;
+	opacity: .15;
+	background: #262626;
+	mix-blend-mode: luminosity;
+}
+.chiffres .footer-img .gradient {
+  position: absolute;
+  top: 0;
+  z-index: 5;
+  width: 100%;
+  height: 360px;
+  background-image: linear-gradient(to bottom, #262626, rgba(38, 38, 38, 0));
+}
+
+.chiffres .tagline-green {
+  font-size: 80px;
+  line-height: 84px;
+  font-family: 'rationalextra_bold';
+  margin-top: 250px;
+}
+
+.chiffres .tagline {
   color: #5feb98;
+  font-size: 80px;
+  line-height: 84px;
+  font-family: 'rationalsemi_bold';
+  margin-bottom: 20px;
 }
 
-.chiffres ul li .unit {
-  color: #5feb98;
-}
+@media (max-width:52rem) {
 
-@media (max-width:60rem) {
-  .chiffres {
-    padding-bottom: 300px;
+  .chiffres .number {
+    font-size: 110px;
+	padding-top: 30px;
   }
-
-  .chiffres ul {
-    padding: 0;
-    margin-top: 30px;
+  
+  .chiffres .unit {
+    font-size: 26px;
+	border-bottom: 0;
+	padding-bottom: 0px;
   }
-
-  .chiffres ul li {
-    font-size: 28px;
-    line-height: 32px;
-    font-family: 'rationalbook_bold';
-    margin-bottom: 20px;
+  
+  .chiffres .comment {
+	padding-top: 20px;
+    font-size: 19px;
+	padding-bottom: 30px;
   }
-
-  .chiffres ul li .number {
+  
+  .chiffres .tagline-green {
     font-size: 44px;
+    line-height: 52px;
+    margin-top: 90px;
+  }
+  
+  .chiffres .tagline {
+    font-size: 44px;
+    line-height: 52px;
+    margin-bottom: 25px;
   }
 
 }
 
-.team {
-  margin-top: -700px;
+.contents h3 {
+	margin-bottom: 90px;
+}
+.contents .cell {
+	border: solid 5px #262626;
+	height: 368px;
+	padding: 58px 50px 20px 50px;
+	text-align: center;
+}
+.contents .cell p {
+	margin-top: 25px;
+    font-size: 22px;
+    line-height: 30px;
+    font-family: 'rationalsemi_bold';
+	text-align: center;
+}
+
+@media (max-width:52rem) {
+	.contents h3 {
+		margin-bottom: 40px;
+	}
+	.contents .cell {
+		height: auto;
+		min-height: 288px;
+		margin-bottom: 30px;
+	}
+	.contents .cell p {
+		font-size: 19px;
+		line-height: 24px;
+	}
+}
+
+
+
+.work {
+	background-color: #262626;
+	color: #cdcdcd;
+}
+@media (min-width:60.03rem) {
+	.work .table {
+		border: solid 3px #4c4c4c;
+	}
+}
+.work .cell-header {
+	color: #5feb98;
+	border: solid 3px #4c4c4c;
+}
+.work .cell-header p {
+	line-height: 90px;
+}
+.work .cell {
+	border: solid 3px #4c4c4c;
+	padding: 40px;
+	min-height: 400px;
+}
+.work .cell ul {
+	padding: 0;
+}
+.work .cell ul li {
+  margin-bottom: 10px;
+  padding-left: 45px;
+  list-style-type: none;
+  font-size: 22px;
+  line-height: 32px;
+  font-family: 'rationalsemi_bold';
+  background: url('/assets/arrow_green.svg') no-repeat;
+  background-position: left 10px;
+  background-size: 32px 10px;
+}
+
+@media (max-width:52rem) {
+	.work .cell-header p {
+		font-size: 26px;
+		line-height: 90px;
+	}
+	.work .cell {
+		margin-bottom: 35px;
+		border-top: 0;
+		padding: 20px;
+		min-height: 0;
+	}
+	.work .cell ul li {
+		font-size: 19px;
+		line-height: 24px;
+		background-position: left 5px;
+	}
+}
+
+.work ul.customers {
+	padding: 0;
+	text-align: center;
+}
+
+.work ul.customers  li {
+  list-style-type: none;
+  display: inline-block;
+  padding: 20px;
+}
+
+.work a.btn,.slide .work a.btn:hover, .work a.btn:visited {
+  font-family: 'rationalextra_bold';
+  color: #5feb98;
+  padding: 30px 80px 30px 80px;
+  border: solid 5px #5feb98;
+  font-size: 22px;
+  text-decoration: none;
+  display: inline-block;
+  margin-bottom: 16px;
+  margin-top: 16px;
+  -o-transition:.5s;
+  -ms-transition:.5s;
+  -moz-transition:.5s;
+  -webkit-transition:.5s;
+  transition:.5s;
+}
+.work a.btn:hover {
+	color: #262626;
+	background-color: #5feb98;
+}
+@media (max-width:52rem) {
+  .work a.btn,.slide .work a.btn:hover,.work a.btn:focus,.work a.btn:visited {
+    border: solid 3px #5feb98;
+    padding: 17px 35px 17px 35px;
+	font-size: 17px;
+  }
+}
+
+@media (min-width:52.02rem) {
+	.team {
+		padding-top: 200px;
+		padding-bottom: 200px;
+		position: relative;
+	}
+	.team .white {
+		background-color: #fff;
+		padding: 50px;
+		color: #262626;
+	}
+	.team h2 {
+		margin: 0;
+	}
+	.team h4 {
+		font-size: 28px;
+		line-height: 32px;
+	}
+	.team p {
+		font-size: 22px;
+		line-height: 30px;
+	}
+	.team .cover {
+		z-index: -1;
+		position: absolute;
+		background-blend-mode: color;
+		opacity: .1;
+	}
+}
+
+@media (max-width:52rem) {
+	.team h2 {
+		border-bottom: solid 10px #262626;
+	}
+	.team .photo {
+		min-height: 300px;
+	}
+	.team h4 {
+		font-size: 18px;
+		line-height: 24px;
+	}
+	.team p {
+		font-size: 18px;
+		line-height: 24px;
+	}
 }
 
 .team .photo {
   margin-top: 65px;
 }
 
-@media (max-width:60rem) {
-  .team {
-    margin-top: -300px;
-  }
-
+@media (max-width:52rem) {
   .team .photo {
     margin-top: 30px;
   }
 }
 
+
+.newsletter {
+  background-color: #262626;
+  color: #fff;
+}
+
 .newsletter h2 {
-  border-bottom: solid 20px #262626;
+  margin-bottom: 10px;
 }
 
 .newsletter ul {
   padding: 0;
-  margin-top: 50px;
 }
 
 .newsletter ul li {
@@ -430,14 +737,14 @@ section p {
   padding: 36px 0px 30px 0px;
   list-style-type: none;
   font-size: 30px;
-  font-family: 'rationalbook_bold';
-  border-bottom: solid 5px #262626;
-  background: url('/assets/arrow.png') no-repeat right center;
+  font-family: 'rationalsemi_bold';
+  border-bottom: solid 5px #5feb98;
+  background: url('/assets/arrow_green.svg') no-repeat right center;
 }
 
-@media (max-width:60rem) {
+@media (max-width:52rem) {
   .newsletter h2 {
-    border-bottom: solid 10px #262626;
+    border-bottom: solid 10px #5feb98;
   }
 
   .newsletter ul {
@@ -447,14 +754,13 @@ section p {
   .newsletter ul li {
     margin-top: 5px;
     padding: 10px 0px 10px 0px;
-    font-size: 20px;
-    border-bottom: solid 3px #262626;
-    text-align: center;
-    background: none;
+    font-size: 17px;
+    border-bottom: solid 3px #5feb98;
+	background-size: 38px 12px;
   }
 }
 
-.contact h2, .chiffres h2, .team h2 {
+.contact h2, .chiffres h2, .work h2 {
   color: #fff;
 }
 
@@ -468,11 +774,11 @@ section p {
   padding: 36px 20px 30px 20px;
   list-style-type: none;
   font-size: 30px;
-  font-family: 'rationalbook_bold';
-  background: #5feb98 url('/assets/arrow.png') no-repeat right 20px center;
+  font-family: 'rationalsemi_bold';
+  background: #5feb98 url('/assets/arrow_black.svg') no-repeat right 20px center;
 }
 
-@media (max-width:60rem) {
+@media (max-width:52rem) {
   .contact ul {
     padding: 0;
     margin-top: 20px;
@@ -480,9 +786,8 @@ section p {
 
   .contact ul li {
     margin-top: 5px;
-    padding: 10px 0px 10px 0px;
-    font-size: 20px;
-    text-align: center;
-    background: #5feb98;
+    padding: 10px 60px 10px 20px;
+    font-size: 17px;
+	background-size: 38px 12px;
   }
 }
