1db0ebe6bf6604d967a2810db03fb166d9eaaa04
Milos Tosic <milostosic1125@outlook.com>
2019-02-15 09:39:18 +0100

    rename StoryPages controller to StoryPageTemplates

--- app/Http/Controllers/API/StoryPageTemplatesController.php
+++ app/Http/Controllers/API/StoryPageTemplatesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class StoryPagesController extends Controller
+class StoryPageTemplatesController extends Controller
 {
     //
 }
