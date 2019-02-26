b8ed44ce4cf649b06e1fbd5196322b594d938bb4
xinhonglee <xinhong1010@outlook.com>
2019-02-26 13:41:09 +0100

    Fix controller

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
-class NewsletterController extends BaseController
+class NewsletterController extends Controller
 {
     public function subscribe(Request $request) {
       if ($request->isMethod('post')) {
