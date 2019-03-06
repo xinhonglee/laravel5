ee5aa721b3b441e02cc77fde5d051a5439393b88
Milos Tosic <milostosic1125@outlook.com>
2019-03-06 18:17:42 +0100

    rename image to img in story-layer blade

--- resources/views/story-layer.blade.php
+++ resources/views/story-layer.blade.php
@@ -1,6 +1,6 @@
 <amp-story-grid-layer template="{{ $layer['template'] }}">
   @foreach ($layer['elements'] as $element)
-      @includeWhen($element['type']=='image', 'components.img', ['element' => $element, 'properties' => $element['properties']])
+      @includeWhen($element['type']=='img', 'components.img', ['element' => $element, 'properties' => $element['properties']])
       @includeWhen($element['type']=='richtext', 'components.richtext', ['element' => $element, 'properties' => $element['properties']])
       @includeWhen($element['type']=='video', 'components.video', ['element' => $element, 'properties' => $element['properties']])
   @endforeach
