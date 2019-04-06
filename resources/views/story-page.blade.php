e411c6344de2dfa0475e67793e473389583fd409
Milos Tosic <milostosic1125@outlook.com>
2019-04-06 11:34:32 +0200

    update story page blade

--- resources/views/story-page.blade.php
+++ resources/views/story-page.blade.php
@@ -1,7 +1,7 @@
 @if (!isset($page_id) || $page_id == $page['id'])
 <amp-story-page id = "{{ $page['id'] }}"
-  @if (!empty($page['auto-advance-after']))auto-advance-after = "{{ $page['auto-advance-after'] }}s"@endif
-  @if (!empty($page['background-audio']))background-audio = "{{ $page['background-audio'] }}"@endif>
+  @if (!empty($page['auto-advance-after'])) auto-advance-after="{{ $page['auto-advance-after'] }}s"@endif
+  @if (!empty($page['background-audio'])) background-audio="{{ $page['background-audio'] }}"@endif>
   @each('story-grid-layer', $page['layers'], 'layer')
   @if (!empty($page['cta_layer']))
     @include('story-cta-layer', ['cta_layer' => $page['cta_layer']])
