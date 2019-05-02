f594b27c626601832b04b1dbee37d225f9d57763
Milos Tosic <milostosic1125@outlook.com>
2019-05-02 16:48:16 +0200

    fixed babel issues and ckeditor integration

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,5 +11,65 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+
+const CKEStyles = require('@ckeditor/ckeditor5-dev-utils').styles;
+const CKERegex = {
+  svg: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+  css: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css/,
+};
+
+Mix.listen('configReady', webpackConfig => {
+  const rules = webpackConfig.module.rules;
+  const targetSVG = /(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/;
+  const targetCSS = /\.css$/;
+
+  // exclude CKE regex from mix's default rules
+  // if there's a better way to loop/change this, open to suggestions
+  for (let rule of rules) {
+    if (rule.test.toString() === targetSVG.toString()) {
+      rule.exclude = CKERegex.svg;
+    }
+    else if (rule.test.toString() === targetCSS.toString()) {
+      rule.exclude = CKERegex.css;
+    }
+  }
+});
+
+mix.webpackConfig({
+  plugins: [
+    new CKEditorWebpackPlugin({
+      language: 'en'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: CKERegex.svg,
+        use: [ 'raw-loader' ]
+      },
+      {
+        test: CKERegex.css,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              singleton: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: CKEStyles.getPostCssConfig({
+              themeImporter: {
+                themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+              },
+              minify: true
+            })
+          },
+        ]
+      }
+    ]
+  }
+});
+
 mix.js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css');
