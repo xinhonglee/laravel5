26c1c664402b0e6f0b861a104f311c77d3f727c6
Milos Tosic <milostosic1125@outlook.com>
2019-05-21 09:47:18 +0200

    implement story-bookend ui

--- resources/js/views/Story/components/Settings/StoryBookend.vue
+++ resources/js/views/Story/components/Settings/StoryBookend.vue
@@ -8,7 +8,7 @@
         </ul>
         <div class="clearfix"></div>
         <div class="story-bookend_components mt-3" :class="{flex: components.length === 0}">
-            <div v-if="components.length > 0" class="components mb-3">
+            <div v-if="components.length > 0 && renderComponents" class="components mb-3">
                 <div v-for="(comp, compIndex) of components" :index="compIndex"
                      class="story-bookend_component">
                     <div class="component-icon mt-3">
@@ -18,10 +18,10 @@
                         <dynamic-bookend-component :type="getUcString(comp.slug)"/>
                     </div>
                     <div class="component-tools mt-3">
-                        <span @click="orderUpComponent(compIndex)">
+                        <span v-if="isOrderUp(compIndex)" @click="orderUpComponent(compIndex)">
                             <md-icon style="transform: rotate(-90deg);">trending_flat</md-icon>
                         </span>
-                        <span @click="orderDownComponent(compIndex)">
+                        <span v-if="isOrderDown(compIndex)" @click="orderDownComponent(compIndex)">
                             <md-icon style="transform: rotate(90deg);">trending_flat</md-icon>
                         </span>
                         <span @click="removeComponent(compIndex)">
@@ -67,30 +67,89 @@
           { 'slug': 'line', 'value': false }
         ],
         components: [],
-        availableComponents: constants.bookend.components
+        availableComponents: constants.bookend.components,
+        renderComponents: true,
       }
     },
     methods: {
+      /**
+       * add a component to bookend
+       */
       addComponent () {
         if (this.components.length < this.availableComponents.length) {
-          this.components.push(this.availableComponents[this.components.length]);
+          for(let comb of this.availableComponents) {
+            if(this.components.filter(item => { return item.slug === comb.slug }).length === 0){
+              this.components.push(comb);
+              break;
+            }
+          }
         }
       },
+      /**
+       * remove a component from bookend
+       * @param index
+       */
       removeComponent (index) {
         this.components.splice(index, 1);
       },
+      /**
+       * order up selected component
+       * @param index
+       */
       orderUpComponent(index) {
-
+        this.renderComponents = false;
+        const temp = this.components[index - 1];
+        this.components[index - 1] = this.components[index];
+        this.components[index] = temp;
+        this.$nextTick(() => {
+          this.renderComponents = true;
+        });
       },
+      /**
+       * order down selected component
+       * @param index
+       */
       orderDownComponent (index) {
-
+        this.renderComponents = false;
+        const temp = this.components[index + 1];
+        this.components[index + 1] = this.components[index];
+        this.components[index] = temp;
+        this.$nextTick(() => {
+          this.renderComponents = true;
+        });
       },
+      /**
+       * get share provider icon from given slug
+       * @param slug
+       * @returns {string}
+       */
       getShareProviderIcon (slug) {
         const share = utils.getBookendShareProvider(slug);
         return share ? share.icon : ''
       },
+      /**
+       * get UpperCase string from given string
+       * @param str
+       * @returns {string}
+       */
       getUcString (str) {
         return str.charAt(0).toUpperCase() + str.slice(1);
+      },
+      /**
+       * Is OrderUp available
+       * @param index
+       * @returns {boolean}
+       */
+      isOrderUp(index) {
+        return this.components.length > 1 && index > 0;
+      },
+      /**
+       * Is OrderDown available
+       * @param index
+       * @returns {boolean}
+       */
+      isOrderDown(index) {
+        return this.components.length > 1 && index < (this.components.length - 1);
       }
     }
   }
