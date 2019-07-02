b3f4bb7cb8d3650dc8a6eec725ea679bb909b3dd
xinhonglee <xinhong1010@outlook.com>
2019-07-02 14:59:04 +0200

    + json-ld for stories

--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -5,6 +5,32 @@
     <title>{{ $story['title'] }}</title>
 
     <link rel="canonical" href="{{ $story['slug'] }}">
+    
+    <script type=application/ld+json>{
+        "@context": "http://schema.org",
+        "@type": "NewsArticle",
+        "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": "{{ $story['slug'] }}"
+        },
+        "headline": "{{ $story['title'] }}",
+        "description": "{{ $story['title'] }}",
+        "image": [
+            "{{ $story['poster-portrait-src'] }}"
+        ],
+        "datePublished": "{{$story->ISODate}}",
+        "dateModified": "{{$story->ISODate}}",
+        "publisher": {
+            "@type": "Organization",
+            "name": "Loopsider",
+            "logo": {
+                "@type": "ImageObject",
+		            "url": "{{ URL::to('/') }}/assets/logo.png",
+		            "width": 210,
+		            "height": 210
+            }
+        }
+    }</script>
 
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
