31a28eacb8dc0297fa858e2c52142684d4994efd
xinhonglee <xinhong1010@outlook.com>
2019-07-10 14:52:58 +0200

    positioning added to image

--- resources/js/views/Story/components/Properties/DesignImage.vue
+++ resources/js/views/Story/components/Properties/DesignImage.vue
@@ -1,88 +1,115 @@
 <template>
-    <div>
-        <md-field>
-            <label>Width</label>
-            <md-input v-model="elementWidth"></md-input>
-        </md-field>
-        <md-field>
-            <label>Height</label>
-            <md-input v-model="elementHeight"></md-input>
-        </md-field>
-        <md-field>
-            <label>Layout</label>
-            <md-select v-model="elementLayout">
-                <md-option v-for="(opt, index) in layoutOptions" :value="opt.slug" :key="index">
-                    {{ opt.name }}
-                </md-option>
-            </md-select>
-        </md-field>
-        <md-field>
-            <label>Position</label>
-            <md-select v-model="elementPosition">
-                <md-option v-for="(opt, index) in layoutPosition" :value="opt.slug" :key="index">
-                    {{ opt.name }}
-                </md-option>
-            </md-select>
-        </md-field>
-    </div>
+  <div>
+    <md-field>
+      <label>Width</label>
+      <md-input v-model="elementWidth"></md-input>
+    </md-field>
+    <md-field>
+      <label>Height</label>
+      <md-input v-model="elementHeight"></md-input>
+    </md-field>
+    <md-field>
+      <label>Layout</label>
+      <md-select v-model="elementLayout">
+        <md-option
+          v-for="(opt, index) in layoutOptions"
+          :value="opt.slug"
+          :key="index"
+        >{{ opt.name }}</md-option>
+      </md-select>
+    </md-field>
+    <md-field>
+      <label>Position</label>
+      <md-select v-model="elementPosition">
+        <md-option
+          v-for="(opt, index) in layoutPosition"
+          :value="opt.slug"
+          :key="index"
+        >{{ opt.name }}</md-option>
+      </md-select>
+    </md-field>
+  </div>
 </template>
 
 <script>
-  import constants from '../../../constants';
+import constants from "../../../constants";
 
-  export default {
-    name: "design-image",
-    props: {
-      el: Object
-    },
-    data () {
-      return {
-        layoutOptions: constants.elementLayouts,
-        layoutPosition: constants.positions.img,
-        elementPosition: ""
-      }
-    },
-    computed: {
-      elementWidth: {
-        get () {
-          if (!_.isNil(this.el && this.el.properties && this.el.properties.width)) {
-            return this.el.properties.width;
-          }
-          return '';
-        },
-        set: _.debounce(function (value) {
-          if (this.elementWidth !== value)
-            Vue.$emit('setting:properties', { properties: { width: value } });
-        }, 2000),
+export default {
+  name: "design-image",
+  props: {
+    el: Object
+  },
+  data() {
+    return {
+      layoutOptions: constants.elementLayouts,
+      layoutPosition: constants.positions.img,
+    };
+  },
+  computed: {
+    elementWidth: {
+      get() {
+        if (
+          !_.isNil(this.el && this.el.properties && this.el.properties.width)
+        ) {
+          return this.el.properties.width;
+        }
+        return "";
       },
-      elementHeight: {
-        get () {
-          if (!_.isNil(this.el && this.el.properties && this.el.properties.height)) {
-            return this.el.properties.height;
-          }
-          return '';
-        },
-        set: _.debounce(function (value) {
-          if (this.elementHeight !== value)
-            Vue.$emit('setting:properties', { properties: { height: value } });
-        }, 2000),
-      },
-      elementLayout: {
-        get () {
-          if (!_.isNil(this.el && this.el.properties && this.el.properties.layout)) {
-            return this.el.properties.layout;
-          }
-          return '';
-        },
-        set: _.debounce(function (value) {
-          if (this.elementLayout !== value)
-            Vue.$emit('setting:properties', { properties: { layout: value } });
-        }, 2000),
-      },
+      set: _.debounce(function(value) {
+        if (this.elementWidth !== value)
+          Vue.$emit("setting:properties", { properties: { width: value } });
+      }, 2000)
     },
+    elementHeight: {
+      get() {
+        if (
+          !_.isNil(this.el && this.el.properties && this.el.properties.height)
+        ) {
+          return this.el.properties.height;
+        }
+        return "";
+      },
+      set: _.debounce(function(value) {
+        if (this.elementHeight !== value)
+          Vue.$emit("setting:properties", { properties: { height: value } });
+      }, 2000)
+    },
+    elementLayout: {
+      get() {
+        if (
+          !_.isNil(this.el && this.el.properties && this.el.properties.layout)
+        ) {
+          return this.el.properties.layout;
+        }
+        return "";
+      },
+      set: _.debounce(function(value) {
+        if (this.elementLayout !== value)
+          Vue.$emit("setting:properties", { properties: { layout: value } });
+      }, 2000)
+    },
+
+
+
+
+
+    elementPosition: {
+      get() {
+        if (!_.isNil(this.el && this.el.properties && this.el.properties.verticalPositioning)){
+          return this.el.properties.verticalPositioning;
+        }
+        return "top-left";
+      },
+      set: _.debounce(function(value) {
+        if (this.elementPosition !== value)
+          Vue.$emit("setting:properties", {
+            properties: { verticalPositioning: value }
+          });
+      }, 2000)
+    }
   }
+};
 </script>
 
 <style scoped>
-
 </style>
--- resources/views/story-grid-layer.blade.php
+++ resources/views/story-grid-layer.blade.php
@@ -12,9 +12,21 @@
     @foreach ($layer['elements'] as $element)
     <div style="position:relative" @if (!empty($element['grid-area'])) grid-area="{{ $element['grid-area']}}" @endif>
         <div
+            {{-- Richtext positioning --}}
             @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='top') style="position:absolute;top:0%;width:100%" @endif
             @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='middle') style="position:absolute;top:50%;transform:translateY(-50%);width:100%" @endif
             @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='bottom') style="position:absolute;top:100%;transform:translateY(-100%);width:100%" @endif
+
+            {{-- Image positioning --}}
+            @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='top-left') style="text-align:left;position:absolute;top:0%;width:100%" @endif
+            @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='middle-left') style="text-align:left;position:absolute;top:50%;transform:translateY(-50%);width:100%" @endif
+            @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='bottom-left') style="text-align:left;position:absolute;top:100%;transform:translateY(-100%);width:100%" @endif
+            @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='top-center') style="text-align:center;position:absolute;top:0%;width:100%" @endif
+            @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='middle-center') style="text-align:center;position:absolute;top:50%;transform:translateY(-50%);width:100%" @endif
+            @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='bottom-center') style="text-align:center;position:absolute;top:100%;transform:translateY(-100%);width:100%" @endif
+            @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='top-right') style="text-align:right;position:absolute;top:0%;width:100%" @endif
+            @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='middle-right') style="text-align:right;position:absolute;top:50%;transform:translateY(-50%);width:100%" @endif
+            @if ( !empty($element['properties']['verticalPositioning']) && $element['properties']['verticalPositioning']=='bottom-right') style="text-align:right;position:absolute;top:100%;transform:translateY(-100%);width:100%" @endif
         >
             @includeWhen($element['type']=='img', 'components.img', ['element' => $element, 'properties' => $element['properties']])
             @includeWhen($element['type']=='richtext', 'components.richtext', ['element' => $element, 'properties' => $element['properties']])
